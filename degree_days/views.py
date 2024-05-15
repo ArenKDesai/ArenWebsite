b13dc1f3927cb317add5a12524a5416c95aba4a4
Aren <flare20012@gmail.com>
2024-05-14 19:18:02 -0500

    Prototype download

--- degree_days/views.py
+++ degree_days/views.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -49,16 +49,20 @@ def download_csv(request):
         start_year = int(request.POST.get('start_year'))
         end_year = int(request.POST.get('end_year'))
         
-        # Generate CSV content
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="degree_days.csv"'
-        
-        writer = csv.writer(response)
-        writer.writerow(['Year', 'Degree Days'])
+        # Generate CSV content using pandas
+        data = {'Year': [], 'Degree Days': []}
         for year in range(start_year, end_year + 1):
             # Calculate degree days for each year (example: dummy value)
             degree_days = year * 10  
-            writer.writerow([year, degree_days])
+            data['Year'].append(year)
+            data['Degree Days'].append(degree_days)
+        
+        df = pd.DataFrame(data)
+        
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="degree_days.csv"'
+        
+        df.to_csv(path_or_buf=response, index=False)
         
         return response
     else:
