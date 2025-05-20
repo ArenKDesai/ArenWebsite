e6d8aa700c0e1d09917b1d8f105d76d7d363e138
Aren <arenkdesai@gmail.com>
2025-05-19 21:23:44 -0500

    fixing coloring

--- threejs-arendesai.com/windowOpen.js
+++ threejs-arendesai.com/windowOpen.js
@@ -166,7 +166,7 @@ function showWebsiteIframe(url) {
   overlay.style.width = '40%';
   overlay.style.height = '60%';
   overlay.style.zIndex = '1000';
-  overlay.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';   
+  // overlay.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';   
   // overlay.style.boxShadow = '0 4px 12px rgba(0, 0, 0, 0.15)';
   overlay.style.borderRadius = '4px';
   overlay.style.overflow = 'hidden';
